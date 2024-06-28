add636e957871ea8893d8254eb352c29e268c625
HenrySu-sudo <Haoransu2022@outlook.com>
2024-06-28 10:40:57 +0800

    remove some bugs

--- src/main/java/org/gatech/usercenter/controller/UserController.java
+++ src/main/java/org/gatech/usercenter/controller/UserController.java
@@ -1,22 +1,21 @@
 package org.gatech.usercenter.controller;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.gatech.usercenter.common.errorCode;
+import org.gatech.usercenter.common.generalRespon;
 import org.gatech.usercenter.entity.User;
 import org.gatech.usercenter.entity.request.userLoginRequest;
 import org.gatech.usercenter.entity.request.userRegisterRequest;
 import org.gatech.usercenter.exception.businessException;
 import org.gatech.usercenter.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.gatech.usercenter.utils.resultutils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.gatech.usercenter.constant.userConstant.USER_LOGIN_STATE;
 
@@ -29,7 +28,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public long userRegister(@RequestBody userRegisterRequest userRegisterRequest){
+    public generalRespon<Long> userRegister(@RequestBody userRegisterRequest userRegisterRequest){
 
         if(userRegisterRequest == null) throw new businessException(errorCode.NOT_LOGIN);
         String userName=userRegisterRequest.getUserName();
@@ -40,11 +39,11 @@ public class UserController {
 
         long res=userService.userRegister(userName,userPassword,checkedPassword);
 
-        return res;
+        return resultutils.success(res);
     }
 
     @PostMapping("/login")
-    public User userdoLogin(@RequestBody userLoginRequest userLoginRequest, HttpServletRequest httpServletRequest){
+    public generalRespon<User> userdoLogin(@RequestBody userLoginRequest userLoginRequest, HttpServletRequest httpServletRequest){
 
         if(userLoginRequest==null) throw new businessException(errorCode.NULL_ERROR);
         String userName=userLoginRequest.getUserName();
@@ -56,33 +55,33 @@ public class UserController {
 
         User user = userService.userLogin(userName, userPassword, httpServletRequest);
 
-        return user;
+        return resultutils.success(user);
     }
 
     @GetMapping("/search")
-    public List<User> userSearch(String userName,HttpServletRequest httpServletRequest){
-        return userService.userSearch(userName,httpServletRequest);
+    public generalRespon<List<User>> userSearch(String userName, HttpServletRequest httpServletRequest){
+        return resultutils.success(userService.userSearch(userName,httpServletRequest));
     }
 
     @GetMapping("/delete")
-    public boolean deleteUser(long id,HttpServletRequest httpServletRequest){
-        return userService.deleteUser(id,httpServletRequest);
+    public generalRespon<Boolean> deleteUser(long id, HttpServletRequest httpServletRequest){
+        return resultutils.success(userService.deleteUser(id,httpServletRequest));
     }
 
     @RequestMapping("/current")
-    public User currentUser(HttpServletRequest httpServletRequest){
+    public generalRespon<User> currentUser(HttpServletRequest httpServletRequest){
         User currentUser = (User) httpServletRequest.getSession().getAttribute(USER_LOGIN_STATE);
         long id=currentUser.getId();
         User user=userService.getById(id);
-        return userService.hideUserInfo(user);
+        return resultutils.success(userService.hideUserInfo(user));
     }
 
     @PostMapping("/logout")
-    public Integer userLogout(HttpServletRequest httpServletRequest){
+    public generalRespon<Integer> userLogout(HttpServletRequest httpServletRequest){
         if(httpServletRequest==null){
             throw new businessException(errorCode.PARAMS_ERROR);
         }
         httpServletRequest.getSession().setAttribute(USER_LOGIN_STATE,null);
-        return 1;
+        return resultutils.success(1);
     }
 }
