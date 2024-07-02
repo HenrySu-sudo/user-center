2c924321780401534632cb35337266b2311708c1
HenrySu-sudo <Haoransu2022@outlook.com>
2024-07-02 21:02:54 +0800

    some small changes

--- src/main/java/org/gatech/usercenter/controller/UserController.java
+++ src/main/java/org/gatech/usercenter/controller/UserController.java
@@ -22,7 +22,7 @@ import static org.gatech.usercenter.constant.userConstant.USER_LOGIN_STATE;
 @RestController
 @RequestMapping("/user")
 @Slf4j
-@CrossOrigin(origins = "http://223.4.215.83/")
+@CrossOrigin(origins = "http://223.4.215.83")
 public class UserController {
 
     @Resource
